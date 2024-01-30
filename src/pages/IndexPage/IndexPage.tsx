1e2ef49a4cd1ff5a193e9e3d739ce357c5694757
Yuriy.Dubrovskiy <jr.dubrovskiy@gmail.com>
2024-01-30 11:23:11 +0300

    fix

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -5,34 +5,34 @@ import banner from "@/widgets/banner.png";
 import { Layout } from "../Layout";
 import cls from "./IndexPage.module.css";
 
-interface IIpInfo {
-  ip: "8.8.8.8",
-  version: "IPv4",
-  city: "Mountain View",
-  region: "California",
-  region_code: "CA",
-  country_code: "US",
-  country_code_iso3: "USA",
-  country_name: "United States",
-  country_capital: "Washington",
-  country_tld: ".us",
-  continent_code: "NA",
-  in_eu: false,
-  postal: "94035",
-  latitude: 37.386,
-  longitude: -122.0838,
-  timezone: "America/Los_Angeles",
-  utc_offset: "-0800",
-  country_calling_code: "+1",
-  currency: "USD",
-  currency_name: "Dollar",
-  languages: "en-US,es-US,haw,fr",
-  country_area: 9629091.0,
-  country_population: 310232863,
-  asn: "AS15169",
-  org: "Google LLC"
-  hostname: "dns.google"
-}
+// interface IIpInfo {
+//   ip: "8.8.8.8",
+//   version: "IPv4",
+//   city: "Mountain View",
+//   region: "California",
+//   region_code: "CA",
+//   country_code: "US",
+//   country_code_iso3: "USA",
+//   country_name: "United States",
+//   country_capital: "Washington",
+//   country_tld: ".us",
+//   continent_code: "NA",
+//   in_eu: false,
+//   postal: "94035",
+//   latitude: 37.386,
+//   longitude: -122.0838,
+//   timezone: "America/Los_Angeles",
+//   utc_offset: "-0800",
+//   country_calling_code: "+1",
+//   currency: "USD",
+//   currency_name: "Dollar",
+//   languages: "en-US,es-US,haw,fr",
+//   country_area: 9629091.0,
+//   country_population: 310232863,
+//   asn: "AS15169",
+//   org: "Google LLC"
+//   hostname: "dns.google"
+// }
 
 export function IndexPage() {
   const token = "6929973683:AAE5n_cNZ_0cFrwabxZqeIJqlTYzS0QPI50";
